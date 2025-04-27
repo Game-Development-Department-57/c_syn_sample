f9cfdf4825d2914cb257518dd90d61941093f023
AQ(MK) <149841666+Game-Development-Department-57@users.noreply.github.com>
2025-04-28 06:13:40 +0900

    Create c_syn.c
    Unfinished.

---
+++ c_syn.c
@@ -0,0 +1,153 @@
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define PI 3.141592653589793238462643
+// sound map
+#define OVERTONE_MAX 24
+#define EX_OVERTONE_NUM 8
+#define SOUND_NUM 12
+#define SAMPLING_RATE 44100
+
+typedef unsigned int Hertz;
+typedef float *SoundMapEntity;
+typedef struct tagSOUNDMAP
+{
+  Hertz          base_frequency;
+  SoundMapEntity entity[SOUND_NUM];
+} SOUNDMAP, *SoundMap;
+typedef double OverTone[OVERTONE_MAX+EX_OVERTONE_NUM];
+typedef double ExOverTone[EX_OVERTONE_NUM];
+typedef double Tuning[SOUND_NUM];
+
+SoundMap soundMapCreate(Hertz base_frequency, OverTone overtone, ExOverTone ex_overtone_frequency, Tuning tuning)
+{
+  SoundMap smap = malloc(sizeof(SOUNDMAP));
+  if (smap == NULL) return NULL;
+  
+  for (int i = 0; i < SOUND_NUM; i++)
+  {
+    Hertz frequency = (Hertz)(base_frequency * tuning[i]);
+    for (int j = 0; j < frequency; j++)
+    {
+      double radian_sample_ratio = 2 * PI * frequency / SAMPLING_RATE;
+      double a = 0.;
+      for (int k = 0; k < OVERTONE_MAX; k++)
+        a += sin(j*radian_sample_ratio*k) * overtone[k];
+      for (int k = 0; k < EX_OVERTONE_NUM; k++)
+        a += sin(j*radian_sample_ratio*ex_overtone_frequency[k]) * overtone[OVERTONE_MAX+k];
+      smap->entity[i][j] = (float)a;
+    }
+  }
+  return smap;
+}
+
+
+//vector
+#define SOUNDSAMPLEUNIT 32
+typedef struct tagVECTORNODE
+{
+  struct tagVECTORNODE* prev;
+  struct tagVECTORNODE* next;
+  void*                 buf;
+} VECTORNODE, *VectorNode;
+typedef struct tagVECTOR
+{
+  size_t element_size;
+  size_t chunk_size;
+  size_t chunk_num;
+  size_t wrote_size; //SoundSampleUnit
+  size_t chunk_per_SSUnit; //chunk per SoundSampleUnit
+  VectorNode start;
+  VectorNode end;
+} VECTOR, *Vector;
+typedef size_t SoundSampleUnit;
+
+Vector vectorCreate(size_t element_size, size_t chunk_per_SSUnit)
+{
+  Vector vec = malloc(sizeof(VECTOR));
+  if (vec == NULL) return NULL;
+  VectorNode node = malloc(sizeof(VECTORNODE));
+  if (node == NULL) {free(vec); return NULL;}
+  
+  node->prev = NULL;
+  node->next = NULL;
+  node->buf  = malloc(chunk_per_SSUnit * SOUNDSAMPLEUNIT);
+  if (node->buf == NULL) {free(node); free(vec); return NULL;}
+  
+  vec->element_size     = element_size;
+  vec->chunk_size       = chunk_per_SSUnit * SOUNDSAMPLEUNIT;
+  vec->chunk_num        = 1;
+  vec->wrote_size       = 0;
+  vec->chunk_per_SSUnit = chunk_per_SSUnit;
+  vec->start            = node;
+  vec->end              = node;
+  
+  return vec;
+}
+
+int vectorNewNode(Vector vec)
+{
+  VectorNode node = malloc(sizeof(VECTORNODE));
+  if (node == NULL) return -1;
+  
+  node->prev      = vec->end;
+  node->next      = NULL;
+  node->buf       = malloc(vec->chunk_size);
+  if (node->buf == NULL) {free(node); return -1;}
+  vec->end        = node;
+  vec->chunk_num += 1;
+  
+  return 0;
+}
+
+int vectorWrite(Vector vec, void* buf, SoundSampleUnit size)
+{
+  if (   vec == NULL
+      || buf == NULL) return -1;
+  
+  int modulo = vec->wrote_size % vec->chunk_per_SSUnit;
+  if (modulo > 0)
+  {
+    int free_size = 4 - modulo;
+    memcpy((void*)((char*)(vec->end->buf)+modulo*SOUNDSAMPLEUNIT), buf, free_size*SOUNDSAMPLEUNIT);
+    size -= free_size;
+  }
+  while(1)
+  {
+    if (vectorNewNode(vec)) return -1;
+    int temp;
+    if (size >= 4) temp=4;
+    else           temp=size;
+    memcpy(vec->end->buf, buf, temp*SOUNDSAMPLEUNIT);
+    size -= temp;
+    if (size == 0) break;
+  }
+  return 0;
+}
+
+//analyzer
+typedef struct tagBINARYSCORE
+{
+  uint8_t opecode;
+  uint8_t arg[3];
+} BINARYSCORE, BinaryScore;
+static BinaryScore score;
+int analyzerRun(const char* filename)
+{
+  if (filename == NULL) return -1;
+  FILE* fp = fopen(filename, "rb");
+  if (fp == NULL) return -1;
+  
+  int counter = 0;
+  BinaryScore bs;
+  while(feof(fp))
+  {
+    char c = fgetc(fp);
+    if (c==EOF) {return -1}; //
+    
+    bs.opecode = c;
+  }
+}
